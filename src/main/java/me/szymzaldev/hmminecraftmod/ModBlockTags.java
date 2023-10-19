10370451c83d609f337008a61322d993a106a306
Szymzal <szymzal05@gmail.com>
2023-10-19 17:51:21 +0200

    Change namespace of mod's block tags

--- src/main/java/me/szymzaldev/hmminecraftmod/ModBlockTags.java
+++ src/main/java/me/szymzaldev/hmminecraftmod/ModBlockTags.java
@@ -5,6 +5,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
+import static me.szymzaldev.hmminecraftmod.HMMinecraftMod.MOD_ID;
+
 public class ModBlockTags {
 
     public static final TagKey<Block> NOT_BREAKABLE_BY_WATER = create("not_breakable_by_water");
@@ -12,7 +14,7 @@ public class ModBlockTags {
     private ModBlockTags() {}
 
     private static TagKey<Block> create(String name) {
-        return TagKey.create(Registries.BLOCK, new ResourceLocation(name));
+        return TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, name));
     }
 
 }
